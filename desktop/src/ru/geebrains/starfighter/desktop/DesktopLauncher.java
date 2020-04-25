4ecf5661bee621efb623a742ea6462cf0d84c8f4
Andrei Krylov <krilovavit@mail.ru>
2020-04-25 14:33:22 +0300

    initial commit

---
+++ desktop/src/ru/geebrains/starfighter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geebrains.starfighter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geebrains.starfighter.StarFighter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarFighter(), config);
+	}
+}
