f4d6aea7f778715e96b85470e35bca9882fe9469
Andrei Krylov <krilovavit@mail.ru>
2020-04-30 20:43:34 +0300

    баг с остановкой так и не решен

--- core/src/ru/geebrains/starfighter/screen/MenuScreen.java
+++ core/src/ru/geebrains/starfighter/screen/MenuScreen.java
@@ -20,14 +20,14 @@ public class MenuScreen extends BaseScreen {
     private final Vector2 shipCenter = new Vector2(spaceShipWidth / 2.0f, spaceShipHeight / 2.0f);
 
     private Vector2 shipCoordinates;
-    private Vector2 destinationCoordinates;
     private Vector2 shipSpeed;
     private Vector2 touch;
     private Vector2 distance;
+    private Vector2 direction;
 
     private final float SHIP_MAXSPEED = 50.0f;
-    private final float SHIP_MINSPEED = 1.0f;
-    private final float SHIP_ACCEL = 0.25f;
+    private final float SHIP_MINSPEED = 2.0f;
+    private final float SHIP_ACCEL = 0.5f;
 
     private BitmapFont font2;
 
@@ -36,11 +36,11 @@ public class MenuScreen extends BaseScreen {
         super.show();
         backgroundMain = new Texture("backgroundGame.jpg");
         playerShip = new Texture("playerSpaceship.png");
-        touch = new Vector2();
         shipSpeed = new Vector2();
-        destinationCoordinates = new Vector2((Gdx.graphics.getWidth() - spaceShipWidth) / 2.0f, (Gdx.graphics.getHeight() - spaceShipHeight) / 2.0f);
-        shipCoordinates = new Vector2(destinationCoordinates.x, destinationCoordinates.y);
-
+        touch = new Vector2((Gdx.graphics.getWidth() - spaceShipWidth) / 2.0f, (Gdx.graphics.getHeight() - spaceShipHeight) / 2.0f);
+        shipCoordinates = new Vector2(touch);
+        distance = new Vector2();
+        direction = new Vector2();
         font2 = new BitmapFont();
         font2.setColor(Color.GREEN);
 
@@ -53,13 +53,12 @@ public class MenuScreen extends BaseScreen {
 
         batch.begin();
         batch.draw(backgroundMain, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        font2.draw(batch, String.format("Curr: X=%s, Y=%s", shipCoordinates.x, shipCoordinates.y), 10, 20);
-        font2.draw(batch, String.format("Dest: X=%s, Y=%s", destinationCoordinates.x, destinationCoordinates.y), 10, 35);
+
         Sprite sprite = new Sprite(playerShip);
 
-        if (destinationCoordinates.x > shipCoordinates.x) {
+        if (touch.x > shipCoordinates.x) {
             isShipFlipped = true;
-        } else if (destinationCoordinates.x < shipCoordinates.x) {
+        } else if (touch.x < shipCoordinates.x) {
             isShipFlipped = false;
         }
         sprite.flip(isShipFlipped, false);
@@ -77,38 +76,34 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        touch.set(screenX , Gdx.graphics.getHeight() - screenY);
-        destinationCoordinates = touch.sub(shipCenter);
         shipSpeed.set(0, 0);
+        touch.set(screenX , Gdx.graphics.getHeight() - screenY).sub(shipCenter);;
         return false;
     }
 
     private void calculateShipCoordinates() {
 
-            if (shipCoordinates != destinationCoordinates) {
+            if (shipCoordinates != touch) {
 
-                shipCoordinates.add(shipSpeed);
+                distance.set(touch).sub(shipCoordinates);
+                direction.set(distance).nor();
 
-                distance = destinationCoordinates.cpy().sub(shipCoordinates);
-                Vector2 direction = distance.cpy().nor();
-
-                if (distance.len() < SHIP_MINSPEED) {
-                    shipSpeed.set(0,0);
-                    shipCoordinates.set(destinationCoordinates);
+                if (distance.len() <=  SHIP_MINSPEED) {
+                    shipCoordinates.set(touch);
                     return;
                 }
 
-                if (distance.len() >= shipSpeed.len() * shipSpeed.len() / (2 * SHIP_ACCEL)) {
+                if (distance.len() > Math.pow(shipSpeed.len(), 2) / (2 * SHIP_ACCEL)) {
                     if (shipSpeed.len() < SHIP_MAXSPEED) {
                         shipSpeed.add(direction.scl(SHIP_ACCEL));
                     }
                 } else {
-                    if (shipSpeed.len() >= SHIP_MINSPEED) {
+                    if (shipSpeed.len() > SHIP_MINSPEED) {
                         shipSpeed.sub(direction.scl(SHIP_ACCEL));
                     }
                 }
 
-
+                shipCoordinates.add(shipSpeed);
             }
     }
 }
